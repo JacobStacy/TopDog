23327242dd9535c058e7e815113cdf6870c275de
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2024-09-20 10:36:55 -0700

    lint fix

---
+++ app/home/profile/add-dog.tsx
@@ -0,0 +1,9 @@
+import s from "./add-dog.module.scss"
+
+export default function AddDog() {
+    return (
+        <div className={s.add_dog}>
+            
+        </div >
+    )
+}
--- app/home/profile/page.tsx
+++ app/home/profile/page.tsx
@@ -1,6 +1,6 @@
 // import ProfileCard from "@/app/ui/home/profile/profile-card"
 import PlusCard from "@/app/ui/home/profile/plus-card"
-import styles from "./page.module.scss"
+// import styles from "./page.module.scss"
 import DogCard from "@/app/ui/home/profile/dog-card"
 
 export default function Profile() {
