332b7389b5d9e9d0e1e675588907ed7bd792e8fa
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2024-09-25 15:22:26 -0700

    wait no I think I got it now

--- app/home/profile/add-dog/page.tsx
+++ app/home/profile/add-dog/page.tsx
@@ -1,6 +1,6 @@
 import AddDogForm from "@/app/ui/home/profile/add-dog/add-dog-form"
 import s from "./page.module.scss"
-// import ImageUploader from "@/app/ui/home/profile/add-dog/image-uploader"
+import ImageUploader from "@/app/ui/home/profile/add-dog/image-uploader"
 import { auth } from "@/auth";
 import { User } from "@/model/user-model";
 
@@ -9,40 +9,40 @@ import { User } from "@/model/user-model";
 export default async function AddDog() {
 
 
+    await dbConnect();
+    const session = await auth();
+    console.log(session?.user?.email);
+    const user = await User.findOne({
+        email: session?.user?.email,
+    });
+
+
+
+    const response = await fetch('/api/get-dogs?getBlank=true');
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch dogs");
+    }
+
+    const data = await response.json();
+
     
-    // const session = await auth();
-    // console.log(session?.user?.email);
-    // const user = await User.findOne({
-    //     email: session?.user?.email,
-    // });
 
-
-
-    // const response = await fetch('/api/get-dogs?getBlank=true');
-
-    // if (!response.ok) {
-    //     throw new Error("Failed to fetch dogs");
-    // }
-
-    // const data = await response.json();
-
-    
-
-    // console.log(user)
-    // // Check if the user exists
-    // if (!user) {
-    //     return (
-    //         <div>
-    //             Busted
-    //         </div>
-    //     )
-    // } else {
+    console.log(user)
+    // Check if the user exists
+    if (!user) {
+        return (
+            <div>
+                Busted
+            </div>
+        )
+    } else {
         return (
             <div className={s.add_dog}>
-                {/* <ImageUploader/> */}
-                {/* <AddDogForm/> */}
+                <ImageUploader/> 
+                <AddDogForm/>
             </div >
         )
-    // }
+    }
 
 }
