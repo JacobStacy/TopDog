e540f48afd43da8eea83bb91c1f3fc37ff2c4329
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2024-10-22 16:53:52 -0700

    SEO updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,33 @@ import { SessionProvider } from "next-auth/react";
 import "./globals.scss";
 
 export const metadata: Metadata = {
-    title: "Top Dog - Find the Best Dogs",
-    description: "Discover the top dogs around and vote for your favorites. A social platform for dog lovers!",
-    keywords: "dogs, top dogs, social media, dog lovers, dog ranking, vote dogs",
+    title: "Top Dog - Discover and Vote for the Best Dogs Online | Dog Lovers Community",
+    description: "Join Top Dog, the platform for dog lovers! Find, rank, and vote for popular dogs. Share your favorites and compete for top rankings!",
+    keywords: "best dogs, vote for dogs, dog lovers social media, rank dogs, popular dogs, top dog platform, find top dogs, dog rankings, dog voting",
     authors: [{ name: "Jacob R. Stacy" }],
     openGraph: {
-        title: "Top Dog - Who's the Top Dog?",
-        description: "Vote for your favorite dogs and see who ranks at the top!",
-        url: "https://top-dog-nine.vercel.app/android-chrome-512x512.png",
+        title: "Top Dog - Who's the Best Dog? Cast Your Vote!",
+        description: "Discover and vote for the most popular dogs! Join a vibrant community of dog lovers, support your favorite pups, and help them climb to the top of the leaderboard.",
+        url: "https://top-dog-nine.vercel.app/",
         type: "website",
         images: [
             {
                 url: "https://top-dog-nine.vercel.app/android-chrome-512x512.png",
                 width: 1200,
                 height: 630,
-                alt: "Top Dog Logo"
+                alt: "Top Dog Logo - Vote for the Best Dogs"
+            }
+        ]
+    },
+    twitter: {
+        card: "summary_large_image",
+        site: "@topdogapp",
+        title: "Top Dog - Vote for the Best Dogs Online",
+        description: "Join the ultimate dog voting platform where you can help rank the best dogs online. Vote for your favorites now!",
+        images: [
+            {
+                url: "https://top-dog-nine.vercel.app/android-chrome-512x512.png",
+                alt: "Top Dog Logo - Join the Dog Voting Fun!"
             }
         ]
     }
@@ -27,13 +39,13 @@ export const metadata: Metadata = {
 const jsonLd = {
     "@context": "https://schema.org",
     "@type": "WebSite",
-    "name": "Top Dog - Find the Best Dogs",
+    "name": "Top Dog - Discover and Rank the Best Dogs",
     "url": "https://top-dog-nine.vercel.app",
     "author": {
         "@type": "Person",
         "name": "Jacob R. Stacy"
     },
-    "description": "Discover the top dogs around and vote for your favorites. A social platform for dog lovers."
+    "description": "Explore Top Dog, where dog lovers unite to discover, rank, and vote for the best dogs. Join the community and help determine the top dog rankings.",
 }
 
 export default async function RootLayout({
@@ -44,7 +56,10 @@ export default async function RootLayout({
     return (
         <html lang="en">
             <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+                <meta name="robots" content="index,follow" />
+                <meta name="theme-color" content="#F2BEBB" />
+                <meta name="author" content="Jacob R. Stacy" />
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
